5062a7614e84c4fce91b48b86121a395ce3a9521
Will-8a <qwertywo361@gmail.com>
2023-08-05 11:11:14 -0400

    Agrega el archivo templates\auth\login.html
    Se agrega la ruta /login al archivo src\app.py

--- src/app.py
+++ src/app.py
@@ -11,6 +11,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('auth/login.html')
+
+
 if __name__ == '__main__':
     # Se selecciona el ambiente
     config_name = 'development'
