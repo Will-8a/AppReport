e5cad30960e51067376ab0742deadf6a2bece4f2
Will-8a <qwertywo361@gmail.com>
2023-08-09 07:43:15 -0400

    Actualiza el archivo src/app.py
    Importa LoginManager del paquete flask_login,
    tambien importa ModelsUser del paquete
    src/models/model_user.py

--- src/app.py
+++ src/app.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask_login import LoginManager
 from flask_mysqldb import MySQL
 from flask_wtf.csrf import CSRFProtect
 
 from config import config
 
+# Models
+from models.model_user import ModelUser
+
 app = Flask(__name__)
 csrf = CSRFProtect()
 mysql = MySQL(app)
+login_manager_app = LoginManager(app)
+
+
+@login_manager_app.user_loader
+def load_user(cedula):
+    return ModelUser.get_by_cedula(mysql, cedula)
 
 
 @app.route('/')
