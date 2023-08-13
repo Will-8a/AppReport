90782e9f1a0233a84b4c91626b5cb42d0307410b
Will-8a <qwertywo361@gmail.com>
2023-08-13 17:20:58 -0400

    Actualiza el archivo src/database/stored_procedures/administrador/administrador.sql
    Agrega las instrucciones para el procedimiento
    almacenado read_usuario_estudiante

--- src/database/stored_procedures/administrador/administrador.sql
+++ src/database/stored_procedures/administrador/administrador.sql
@@ -14,3 +14,22 @@ begin
     insert into estudiante values(ced, id_t, car, can_r, horas_a);
     insert into tutor values (id_t, ced);
 end$
+
+-------------------------------------------------------------------------
+-- Read procedures
+-------------------------------------------------------------------------
+-- Read usuario_estudiante
+delimiter $
+create procedure read_usuario_estudiante(
+    in ced varchar(20)
+)
+begin
+    select usuario.cedula, usuario.nombre_1, usuario.nombre_2, usuario.apellido_p,
+    usuario.apellido_m, usuario.email, usuario.contrasena, usuario.tipo_de_usuario,
+    estudiante.id_tutor, estudiante.carrera, estudiante.cantidad_de_reportes,
+    estudiante.horas_acumuladas
+    from usuario
+    inner join estudiante
+    on usuario.cedula = estudiante.id_estudiante
+    where usuario.cedula = ced;
+end$
