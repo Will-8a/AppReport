05ca247ee3dd2b06a6e87a8c4a85c536639a6a0d
Will-8a <qwertywo361@gmail.com>
2023-08-14 07:34:46 -0400

    Actualiza el archivo src/rest_api/responses.py
    Agrega la funcion usuario_actualizado_en_base_de_datos

--- src/rest_api/responses.py
+++ src/rest_api/responses.py
@@ -110,6 +110,40 @@ def usuario_guardado_en_base_de_datos(tipo_de_usuario):
     return data
 
 
+# Respuesta cuando se ha actualizado un usuario en la base de datos
+def usuario_actualizado_en_base_de_datos(tipo_de_usuario):
+    '''
+    Retorna una respuesta cuando un usuario ha sido actualizado en
+    la base de datos.
+
+    Params:
+    -------
+    tipo_de_usuario : str
+        Una cadena de texto que identifica el tipo de usuario que se ha
+        actualizado en la base de datos
+
+    Returns:
+    -------
+    dict
+        Diccionario con el estado y la respuesta de la actualizacion del
+        usuario. El estado es siempre True y la respuesta contiene un mensaje
+        indicando que el usuario ha sido actualizado y el tipo de respuesta,
+        ademas se agrega un espacio para la variable del tipo de usuario
+    '''
+    data = {
+        'status': True,
+        'response': {
+            'message': 'Usuario {} actualizado en base de datos'.format(
+                tipo_de_usuario.lower()
+            ),
+            'type': 'USER_{}_UPDATE_IN_DATABASE'.format(
+                tipo_de_usuario
+            )
+        }
+    }
+    return data
+
+
 # Respuesta para cuando ocurrio un error interno
 def error_interno():
     '''
