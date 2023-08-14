25be5f150334438ba6a1a8dc9afe134c5a973582
Will-8a <qwertywo361@gmail.com>
2023-08-14 17:28:43 -0400

    Actualiza el archivo src\models\entities\administrador.py
    Agrega la funcion create_usuario_tutor a la
    entidad administrador

--- src/models/entities/administrador.py
+++ src/models/entities/administrador.py
@@ -44,6 +44,39 @@ class Administrador(User):
         # en la base de datos
         return True
 
+    def create_usuario_tutor(self, mysql, datos):
+        datos.update(
+            {
+                'nombre_1': datos.get('nombre_1').upper(),
+                'nombre_2': datos.get('nombre_2').upper(),
+                'apellido_paterno': datos.get('apellido_paterno').upper(),
+                'apellido_materno': datos.get('apellido_materno').upper(),
+                'e_mail': datos.get('e_mail').upper(),
+                'contrasena': self.generar_contrasena(datos.get('contrasena')),
+                'tipo_de_usuario': datos.get('tipo_de_usuario').upper()
+            }
+        )
+        tupla_datos = tuple(datos.values())
+        try:
+            cursor = mysql.connection.cursor()
+
+            # Llamar al procedimiento almacenado
+            cursor.callproc(
+                'create_usuario_tutor',
+                tupla_datos
+            )
+            mysql.connection.commit()
+        except Exception as e:
+            mysql.connection.rollback()
+            print(str(e))
+            raise False
+        finally:
+            # cerrar el cursor
+            cursor.close()
+        # Regresa True si se realizaron los cambios
+        # correctamente en la base de datos
+        return True
+
     def read_usuario_estudiante(self, mysql, cedula):
         try:
             cursor = mysql.connection.cursor()
