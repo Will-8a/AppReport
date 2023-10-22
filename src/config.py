94422961c1809f93eff83f60f96ffd3deb6a08da
Will-8a <qwertywo361@gmail.com>
2023-10-22 09:27:43 -0400

    Actualiza el archivo src/config.py
    Agrega instrucciones para cada ambiente de trabajo

--- src/config.py
+++ src/config.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+
 class Config:
     DEBUG = False
     TESTING = False
@@ -8,6 +16,11 @@ class Config:
 
 class ProductionConfig(Config):
     HOST = '0.0.0.0'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MYSQL_HOST = os.getenv('DATABASE_HOST_UR')
+    MYSQL_USER = os.getenv('DATABASE_USERNAME')
+    MYSQL_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    MYSQL_DB = os.getenv('DATABASE_NAME')
 
 
 class DevelopmentConfig(Config):
@@ -17,12 +30,16 @@ class DevelopmentConfig(Config):
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = ''
-    MYSQL_DB = 'pt_data'
+    MYSQL_DB = 'pt_data_dev'
 
 
 class TestingConfig(Config):
     TESTING = True
-    HOST = '127.0.0.1'
+    HOST = '0.0.0.0'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'pt_data_dev'
 
 
 config = {
